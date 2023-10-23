cd4649ee09c0e466ea842a34b09866e954e64ad0
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-23 21:34:26 +0530

    Create playing_with_digits.py

---
+++ playing_with_digits.py
@@ -0,0 +1,16 @@
+import math
+def dig_pow(n,p):
+    n_list = [int(x) for x in str(n)]
+    power_list = []
+    for i in range(0, len(n_list)):
+        power_list.append(int(math.pow(n_list[i], p+i)))
+    #sum(power_list)/n
+    if (sum(power_list)/n).is_integer():
+        print('k exists')
+        return sum(power_list)//n
+    else:
+        print('k doesnt exist')
+    return power_list
+    return -1
+
+print(dig_pow(46288, 3))
