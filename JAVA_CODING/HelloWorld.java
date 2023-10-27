2aeffcef892bee77c45eb38863db9ac5a3e626dd
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-28 00:40:40 +0530

    update

--- JAVA_CODING/HelloWorld.java
+++ JAVA_CODING/HelloWorld.java
@@ -1,7 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         int i = 0;
-        while(i<1000000){
+        while(i<=4){
         System.out.println("Hi "+ i);
         i++;
         }
