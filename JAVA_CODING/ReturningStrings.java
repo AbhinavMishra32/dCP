eaab2c8ebac93c647846377e29f0a9a61cc4c6f0
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-28 20:45:17 +0530

    update

--- JAVA_CODING/ReturningStrings.java
+++ JAVA_CODING/ReturningStrings.java
@@ -4,6 +4,6 @@ public class ReturningStrings{
     }
 
     public static String greet(String name){
-        return ("Hello, " + name + " how are you doing today?");
+        return ("Hello, " + na  me + " how are you doing today?");
     }
 }
