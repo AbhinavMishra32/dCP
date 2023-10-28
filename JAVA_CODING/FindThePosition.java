e291eae9bc74f50c895d3614c91bc961e11a7460
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-28 21:29:07 +0530

    update

--- JAVA_CODING/FindThePosition.java
+++ JAVA_CODING/FindThePosition.java
@@ -1,3 +1,18 @@
 public class FindThePosition {
-    
+    //private static char[] lowerAsciiStr;
+    static String lowerAsciiStr;
+    public static String position(char alphabet){
+        int poschar;
+        StringBuilder lowercaseAscii = new StringBuilder();
+        for(char c = 'a'; c <='z';c++){ 
+            lowercaseAscii.append(c);
+        
+        }
+        String lowerAsciiStr = lowercaseAscii.toString();
+        //String[] lowerAsciiStrArr = lowerAsciiStr.split("");
+        return "";
+    }
+    public static void main(String[] args){
+        System.out.println(FindThePosition.lowerAsciiStr);
+    }
 }
