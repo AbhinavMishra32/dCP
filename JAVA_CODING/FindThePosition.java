f1ae983d36cbc314e5a67b8aee7b2de44efa2778
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-28 22:50:58 +0530

    update

--- JAVA_CODING/FindThePosition.java
+++ JAVA_CODING/FindThePosition.java
@@ -1,24 +1,32 @@
+import java.util.Arrays;
+
 public class FindThePosition {
     //private static char[] lowerAsciiStr;
     //static String lowerAsciiStr;
-    private static char[] lowerAsciiStrArr;
     public static String position(char alphabet){
-        int posChar;
         StringBuilder lowercaseAscii = new StringBuilder();
         for(char c = 'a'; c <='z';c++){ 
             lowercaseAscii.append(c);
-        
         }
+
         String lowerAsciiStr = lowercaseAscii.toString();
-        String[] lowerAsciiStrArr = lowerAsciiStr.split(""); // Using an empty string as delimiter
+        //String[] lowerAsciiStrArr = lowerAsciiStr.split(""); // Using an empty string as delimiter
+        for (int i = 0;i<lowerAsciiStr.length();i++){
+            char currentChar = lowerAsciiStr.charAt(i);
+        
+        //String finalStr = "Position of alphabet: ";
+        if(currentChar == alphabet){
+            System.out.println("Position of alphabet: " + (i+1));
+        }
+        }
         return "";
     }
 
     public static void main(String[] args){
-        FindThePosition.position('b');
-        
-        System.out.println(FindThePosition.lowerAsciiStr);
-        System.out.println(FindThePosition.lowerAsciiStrArr);
+        //System.out.println(Arrays.toString(FindThePosition.position('b'))); 
+        //System.out.println(FindThePosition.lowerAsciiStr);
+        //System.out.println(FindThePosition.lowerAsciiStrArr);
 
+        FindThePosition.position('c');
     }
 }
--- testing_files/t2.java
+++ testing_files/t2.java
@@ -1,6 +1,8 @@
+import java.util.Arrays;
+
 class t2{
     public static void main(String[] args){
-        int[] testss = [2,3,3,6,7];
-
+        int[] testss = {2,3,3,6,7};
+        System.out.println(Arrays.toString(testss));
     }
 }
