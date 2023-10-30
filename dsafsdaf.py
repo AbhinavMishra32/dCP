3e77858d5f7db4d6e2e9fa4d5c7ea51ef62e1d63
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-30 23:07:06 +0530

    updates

--- dsafsdaf.py
+++ dsafsdaf.py
@@ -1,17 +1,18 @@
 import math
-def sum_dig_pow(a,b):
-    int_range_list = list(range(a,b+1))
+def sum_dig_pow(a, b):
+    int_range_list = list(range(a, b))
     result = []
-    
+
     for i in int_range_list:
         power_list = []
         i_str = str(i)
         for j in range(len(i_str)):
             digit = int(i_str[j])
             power_list.append(math.pow(digit, j + 1))
-    
+
         if i == sum(power_list):
             result.append(i)
+
     return result
 
-print(sum_dig_pow(1,20000))
+print(sum_dig_pow(1, 10))
