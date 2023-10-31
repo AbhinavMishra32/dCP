5fadcde43bf6987b8f643d8afcd95c5f97a7d4e0
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-10-31 23:45:18 +0530

    update

--- JAVA_CODING/FactorialFunction.java
+++ JAVA_CODING/FactorialFunction.java
@@ -1,3 +1,9 @@
 public class FactorialFunction {
-    
+    public static long factorial(int n) {
+        long result =1L;
+        for(int i=0;i<n;i++){
+            result*= (n-i);
+        }
+        return result;
+    }
 }
