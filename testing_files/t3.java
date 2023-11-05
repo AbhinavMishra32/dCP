8a50cbbd2c883c645e9dc5674d9a102cfe80045e
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-11-05 23:11:39 +0530

    update

---
+++ testing_files/t3.java
@@ -0,0 +1,16 @@
+    import java.io.File;
+    import java.io.FileNotFoundException;
+    import java.util.ArrayList;
+    import java.util.Scanner;
+    import StdDraw;
+
+public class t3 {
+
+    public static void main(String[] args) {
+        System.out.println("Solver main is running.");
+
+        StdDraw.setCanvasSize(500, 500);
+        StdDraw.setPenColor(StdDraw.RED);
+        StdDraw.filledRectangle(0,0,10,10);
+     }
+}
