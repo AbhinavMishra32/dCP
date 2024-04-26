f58a8d24cace25789b8ad6e99fd283c4541c9774
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-04-26 17:19:19 +0530

    update

---
+++ CPP_CODING/LL.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+class Node(){
+    public:
+    int data;
+    Node* next;
+
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(Node* &head, int val){
+    Node* n = new Node(val);
+    
+    Node* temp = head;
+    while(temp->next!=NULL){
+        temp->next
+    }
+}
+
+
+
+int main(){
+
+}
