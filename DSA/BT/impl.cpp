72abbddeb6638b46d47a0348b29664d44cbee768
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-12-21 23:51:34 +0530

    BT Implementation

--- DSA/BT/impl.cpp
+++ DSA/BT/impl.cpp
@@ -13,16 +13,48 @@ public:
     right = nullptr;
   }
 
-  void populate(Node* root){
-    bool choice;
-    cout<< "Enter to LEFT of [" << root->value << "] ? ";
+  void populate(){
+    char choice;
+    cout<< "Enter to LEFT or RIGHT of " << this->value << " (l/r): ";
     cin >> choice;
 
+    if (choice == 'l' || choice == 'L'){
+      int value;
+      cout << "Enter value for LEFT child of [" << this->value << "]: "; 
+      cin >> value;
+      this->left = new Node(value);
+      this->left->populate();
+    }
+    cout << "Enter to RIGHT of " << this->value << " (l/r): ";
+    if (choice == 'r' || choice == 'R'){
+      this->left = nullptr;
+      int value;
+      cout << "Enter value for RIGHT child of [" << this->value << "]: ";
+      cin >> value;
+      this->right = new Node(value);
+      this->right->populate();
+    }
+  }
+
+  void display(){
+    cout << this->value << " ";
+    if (this->left != nullptr){
+      this->left->display();
+    }
+    if (this->right != nullptr){
+      this->right->display();
+    }
   }
 };
 
 int main(){
-  Node* root = new Node(10);
+  int rootValue;
+  cout << "Enter root node: ";
+  cin>> rootValue;
+  Node* root = new Node(rootValue);
+  cout << endl;
+  root->populate();
+  root->display();
 
   return 0;
 }
